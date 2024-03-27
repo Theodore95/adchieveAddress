c4d5a6e821f7cf3fd75b92e5d6833b8d8d9a16e8
jeroen van Heiningen <theoprog95@gmail.com>
2024-03-27 12:36:26 +0100

    Address parsing functionality working

---
+++ src/address_parser.py
@@ -0,0 +1,44 @@
+FILEPATH = "src/resources/addresses.txt"
+MAX_SPLITS = 1
+
+def parse_file(filepath: str) -> list[str]:
+  """
+  Parses a text file into a list of strings where newlines occur. In 
+  addition leading/trailing whitespace are removed from entries, as well as empty strings. 
+
+  Args:
+      filepath: The path to the text file.
+
+  Returns:
+      A list of strings, where each element is a stripped line from the file.
+  """
+  with open(filepath, 'r') as file:
+    entries = file.readlines()
+
+  return _clean(_trim(entries))
+
+def _trim(lines: list[str]) -> list[str]:
+  """
+  Iterates over a list of strings and removes leading/trailing whitespace
+  """
+  return list(map(lambda entry: str.strip(entry), lines))
+  # return [entry.strip() for entry in lines]
+
+def _clean(lines: list[str]) -> list[str]:
+  return list(filter(lambda entry: bool(entry), lines))
+
+def split_string(input: str, split_token: str) -> tuple[str, str]:
+  """
+  Splits an input string in to two strings at the first occurrence of split_token.
+
+  Args:
+    input: The string to be split.
+    split_token: the substring where the split is to take place.
+
+  Returns:
+    A two-tuple consisting of the split string or the entire string in the left indice
+    if the substring "split-token" could not be found.
+  """
+  two_parts = input.split(split_token, MAX_SPLITS)
+  if len(two_parts) == 2: return two_parts[0], two_parts[1]
+  else: return two_parts[0], ""
---
+++ tests/address_parser_test.py
@@ -0,0 +1,36 @@
+import unittest 
+import sys
+
+sys.path.append("..")
+from src.address_parser import parse_file, split_string
+
+
+ADDRESS_1 = "Adchieve HQ - Sint Janssingel 92, 5211 DA 's-Hertogenbosch, The Netherlands"
+ADDRESS_2 = "Eastern Enterprise - 46/1 Office no 1 Ground Floor , Dada House , Inside dada silk mills"
+ADDRESS_WITHOUT_HYPHEN = "Neverland, 5225 Figueroa Mountain Road, Los Olivos, Calif. 93441, USA"
+
+ADDRESS_LIST = ["Adchieve HQ - Sint Janssingel 92, 5211 DA 's-Hertogenbosch, The Netherlands", 
+                "Eastern Enterprise - 46/1 Office no 1 Ground Floor , Dada House , Inside dada silk mills"]
+ADDRESSES_FILEPATH = "tests/resources/addresses_reduced.txt"
+
+class TestAddressParsing(unittest.TestCase):
+    
+    """
+    Tests the parse_file and split_string functions in address_parser.
+    """
+    def test_parse_file(self):
+        res = parse_file(ADDRESSES_FILEPATH)
+        self.assertEqual(ADDRESS_LIST, res, )
+
+    def test_split_string(self):
+       res = split_string(ADDRESS_1, " - ")
+       print(res)
+       self.assertEqual(("Adchieve HQ", "Sint Janssingel 92, 5211 DA 's-Hertogenbosch, The Netherlands"), res)
+
+    def test_split_string_without_hyphen(self):
+       res = split_string(ADDRESS_WITHOUT_HYPHEN, " - ")
+       self.assertEqual(("Neverland, 5225 Figueroa Mountain Road, Los Olivos, Calif. 93441, USA", ""), res)
+
+
+if __name__ == "__main__":
+  unittest.main()
